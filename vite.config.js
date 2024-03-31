19efe24ef0d5aa3140a05c1c6387d1b2a6891403
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2024-03-31 07:36:00 +0200

    chore(rollupOptions: configure vite to use second html page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
 // vite.config.js
+const { resolve } = require('path')
 export default {
-    base: '/OgvMitgliederversammlung/'
+    base: '/OgvMitgliederversammlung/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                versammlung2024: resolve(__dirname, "versammlung2024.html"),
+            }
+        },
+    }
 }
